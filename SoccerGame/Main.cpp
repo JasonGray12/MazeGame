1bb2f8808b611a4c941098268657baa4a3502262
akane2014 <akane2002@gmail.com>
2021-11-13 23:13:42 +1300

    Add player movement

--- SoccerGame/Main.cpp
+++ SoccerGame/Main.cpp
@@ -1,8 +1,104 @@
 #include <iostream>
+#include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
+constexpr char WAL = (char)219;
+
+int GetIndexFromCoordinates(int x, int y, int width)
+{
+	return x + y * width;
+}
+
 void main()
 {
-	cout << "@" << endl;
+	int height = 15;
+	int width = 25;
+	int playerX = 1;
+	int playerY = 1;
+
+	char levelArray[]{ '+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'+','-',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','H',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ','V',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','@',' ','|',
+						'|',' ',' ','V',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'+','-',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','H',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ','V',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+', };
+
+	while (1)
+	{
+		//Handle player input
+		char input = ' ';
+		if (_kbhit())
+		{
+			input = _getch();
+		}
+
+		int newPlayerX = playerX;
+		int newPlayerY = playerY;
+		switch (input)
+		{
+		case 'W':
+		case 'w':
+		{
+			newPlayerY--;
+			break;
+		}
+		case 'S':
+		case 's':
+		{
+			newPlayerY++;
+			break;
+		}
+		case 'A':
+		case 'a':
+		{
+			newPlayerX--;
+			break;
+		}
+		case 'D':
+		case 'd':
+		{
+			newPlayerX++;
+			break;
+		}
+		default:
+			break;
+		}
+		playerX = newPlayerX;
+		playerY = newPlayerY;
+
+		//Reset screen
+		HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+		system("cls");
+
+		//Render level
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				int n = GetIndexFromCoordinates(x, y, width);
+				char c = levelArray[n];
+				cout << c;
+			}
+			cout << endl;
+		}
+
+		//Render player
+		COORD actorCursorPosition;
+		actorCursorPosition.X = playerX;
+		actorCursorPosition.Y = playerY;
+		SetConsoleCursorPosition(console, actorCursorPosition);
+		cout << "@";
+	}
+
 }
