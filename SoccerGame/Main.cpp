98cbbd313ef2c66c6d4e6036048d1ee06a20bf64
akane2014 <akane2002@gmail.com>
2021-11-20 23:09:11 +1300

    Add ball rendering and updating

--- SoccerGame/Main.cpp
+++ SoccerGame/Main.cpp
@@ -6,97 +6,142 @@ using namespace std;
 
 constexpr char WAL = (char)219;
 constexpr char BAL = (char)233;
+constexpr char kPlayerSymbol = '@';
+
+void DrawLevel(char level[], int width, int height, int playerX, int playerY, int ballX, int ballY);
+
+int GetIndexFromCoordinates(int x, int y, int width);
+bool UpdatePlayerPosition(char level[], int& playerX, int& playerY, int width, int ballX, int ballY, int& ballMoveX, int& ballMoveY);
+void UpdateBall(int& ballX, int&ballY, int ballMoveX, int ballMoveY);
+
+int main()
+{
+	int height = 10;
+	int width = 15;
+	
+
+	char levelArray[]{  '+','-','-','-','-','-','-','-','-','-','-','-','-','-','+',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'+','-',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'+','-',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
+						'+','-','-','-','-','-','-','-','-','-','-','-','-','-' - '+', };
+
+	int playerX = 1;
+	int playerY = 1;
+	int ballX = 10;
+	int ballY = 5;
+	int ballMoveX = 0;
+	int ballMoveY = 0;
+	bool gameOver = false;
+
+	while (!gameOver)
+	{
+		system("cls");
+		DrawLevel(levelArray, width, height, playerX, playerY, ballX, ballY);
+		UpdateBall(ballX, ballY, ballMoveX, ballMoveY);
+		gameOver = UpdatePlayerPosition(levelArray, playerX, playerY, width, ballX, ballY, ballMoveX, ballMoveY);
+	}
+	system("cls");
+	DrawLevel(levelArray, width, height, playerX, playerY, ballX, ballY);
+	cout << " YOU WON!!" << endl;
+
+	
+
+}
 
 int GetIndexFromCoordinates(int x, int y, int width)
 {
 	return x + y * width;
 }
 
-void main()
+void UpdateBall(int& ballX, int& ballY, int ballMoveX, int ballMoveY)
 {
-	int height = 10;
-	int width = 15;
-	int playerX = 1;
-	int playerY = 1;
+	ballX += ballMoveX;
+	ballY += ballMoveY;
+}
 
-	char levelArray[]{  '+','-','-','-','-','-','-','-','-','-','-','-','-','-','+',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','@','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'+',' ',' ',' ',' ',' ',' ',' ',' ',' ',BAL,' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',
-						'+','-','-','-','-','-','-','-','-','-','-','-','-','-'-'+',
-						
-						
-
-	while (1)
+void DrawLevel(char level[], int width, int height, int playerX, int playerY, int ballX, int ballY)
+{
+	for (int y = 0; y < height; y++)
 	{
-		//Handle player input
-		char input = ' ';
-		if (_kbhit())
+		for (int x = 0; x < width; x++)
 		{
-			input = _getch();
-		}
-
-		int newPlayerX = playerX;
-		int newPlayerY = playerY;
-		switch (input)
-		{
-		case 'W':
-		case 'w':
-		{
-			newPlayerY--;
-			break;
-		}
-		case 'S':
-		case 's':
-		{
-			newPlayerY++;
-			break;
-		}
-		case 'A':
-		case 'a':
-		{
-			newPlayerX--;
-			break;
-		}
-		case 'D':
-		case 'd':
-		{
-			newPlayerX++;
-			break;
-		}
-		default:
-			break;
-		}
-		playerX = newPlayerX;
-		playerY = newPlayerY;
-
-		//Reset screen
-		HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-		system("cls");
-
-		//Render level
-		for (int y = 0; y < height; y++)
-		{
-			for (int x = 0; x < width; x++)
+			if (playerX == x && playerY == y)
 			{
-				int n = GetIndexFromCoordinates(x, y, width);
-				char c = levelArray[n];
-				cout << c;
+				cout << kPlayerSymbol;
 			}
-			cout << endl;
-		}
+			else if (ballX == x && ballY == y)
+			{
+				cout << BAL;
+			}
+			else
+			{
+				int index = GetIndexFromCoordinates(x, y, width);
+				cout << level[index];
+			}
 
-		//Render player
-		COORD actorCursorPosition;
-		actorCursorPosition.X = playerX;
-		actorCursorPosition.Y = playerY;
-		SetConsoleCursorPosition(console, actorCursorPosition);
-		cout << "@";
+		}
+		cout << endl;
 	}
+}
+
+bool UpdatePlayerPosition(char level[], int& playerX, int& playerY, int width, int ballX, int ballY, int& ballMoveX, int& ballMoveY)
+{
+	//Handle player input
+	char input = ' ';
+	if (_kbhit())
+	{
+		input = _getch();
+	}
+
+	int newPlayerX = playerX;
+	int newPlayerY = playerY;
+	switch (input)
+	{
+	case 'W':
+	case 'w':
+	{
+		newPlayerY--;
+		break;
+	}
+	case 'S':
+	case 's':
+	{
+		newPlayerY++;
+		break;
+	}
+	case 'A':
+	case 'a':
+	{
+		newPlayerX--;
+		break;
+	}
+	case 'D':
+	case 'd':
+	{
+		newPlayerX++;
+		break;
+	}
+	default:
+		break;
+	}
+
+	if (newPlayerX == ballX && newPlayerY == ballY)
+	{
+		//todo: decide the ball movement direction here
+		ballMoveX = 1;
+		ballMoveY = 0;
+	}
+
+	playerX = newPlayerX;
+	playerY = newPlayerY;
+
+	cout << '@';
+	return 0;
 
 }
